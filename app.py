cef87b3b6beeace96b0a730c31209a7832dda45e
fizzy-bubalech <av.trumper@gmail.com>
2021-01-02 17:01:43 +0200

    what? u wanna go?

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app.config['SECRET_KEY'] = os.urandom(24)
 def test():
     with open("static/test.txt", "r") as f:
         content = f.read()
-    return(Response(content, mimetype='text/plain'))
+    return('worked')
 
 
 
 if __name__ == "__main__":
-    app.run(debug = True,host='192.168.1.35')
+    app.run(debug = True)
